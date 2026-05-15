a8984a8f084ded75aa2dde731ec7d3bed8bf2fba
zhouyanxi <576053791@qq.com>
2026-05-16 02:40:31 +0800

    [2.1.1] agent重构&表结构解耦

--- main/agent/index.ts
+++
@@ -1,5 +0,0 @@
-export { HISAnalysisAgent, createAgent } from './agent';
-export { ChatSession } from './chatSession';
-export { DeepSeekClient } from './deepseek';
-export * from './types';
-export * from './config';
